c9e01237d84710b89b9d9fa7847f17277b6a6907
jspencerhopkins <jspencerhopkins@gmail.com>
2015-03-06 08:47:29 -0500

    Add Search controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  get "home/index"
+  get "search/index"
+  get 'search' => "search#index"
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
